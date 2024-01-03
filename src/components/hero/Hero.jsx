2aaee9c40cc50b31559d83bc602f3de0aa6e5b9e
BingWeb <154599235+BingWeb@users.noreply.github.com>
2024-01-03 22:26:31 +0700

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 
-import profilePicture from "../../assets/profile.png";
+import profilePicture from "../../assets/profile.jpeg";
 import "./hero.css";
 
 const Hero = () => {
